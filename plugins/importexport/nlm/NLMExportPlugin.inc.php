e0329163ca4d72c676b7397389b84974f58761b5
asmecher <asmecher>
2007-07-23 23:13:13 +0000

    #2883# Code docs and consistency

--- plugins/importexport/nlm/NLMExportPlugin.inc.php
+++ plugins/importexport/nlm/NLMExportPlugin.inc.php
@@ -1,9 +1,13 @@
 <?php
 
 /**
- * NLMExportPlugin.inc.php
+ * @file NLMExportPlugin.inc.php
  *
+ * Copyright (c) 2003-2007 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ * 
  * @package plugins
+ * @class NLMExportPlugin
  *
  * NLM XML metadata export plugin
  *
