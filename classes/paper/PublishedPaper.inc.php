d19bb3557fcedc76d744ec3458e717c8fbebc704
asmecher <asmecher>
2007-12-20 01:02:48 +0000

    #3200# #2883# Back-ported fix for multilingual galley fallback

--- classes/paper/PublishedPaper.inc.php
+++ classes/paper/PublishedPaper.inc.php
@@ -155,13 +155,12 @@ class PublishedPaper extends Paper {
 	 */
 	function &getLocalizedGalleys() {
 		$primaryLocale = Locale::getPrimaryLocale();
-		$locale = Locale::getLocale();
 
 		$allGalleys =& $this->getData('galleys');
 		$galleys = array();
 		foreach (array(Locale::getLocale(), Locale::getPrimaryLocale()) as $tryLocale) {
 			foreach (array_keys($allGalleys) as $key) {
-				if ($allGalleys[$key]->getLocale() == $locale) {
+				if ($allGalleys[$key]->getLocale() == $tryLocale) {
 					$galleys[] =& $allGalleys[$key];
 				}
 			}
