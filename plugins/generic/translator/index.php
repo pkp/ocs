00e311b9503c42f7eaf28a9851076a2e12e9e639
asmecher <asmecher>
2008-03-09 07:17:58 +0000

    #2883# Added translation tools

---
+++ plugins/generic/translator/index.php
@@ -0,0 +1,20 @@
+<?php 
+
+/**
+ * @file index.php
+ *
+ * Copyright (c) 2003-2007 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * Wrapper for translation maintenance plugin.
+ *
+ * @package plugins.generic.translator
+ *
+ * $Id$
+ */
+
+require_once('TranslatorPlugin.inc.php');
+
+return new TranslatorPlugin(); 
+
+?> 
