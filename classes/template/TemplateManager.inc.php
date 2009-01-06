f4fbf359660b3bba5795bdf5859f2e3c801b05f5
asmecher <asmecher>
2009-01-06 23:34:23 +0000

    #3970# Fixed ref bug

--- classes/template/TemplateManager.inc.php
+++ classes/template/TemplateManager.inc.php
@@ -229,7 +229,7 @@ class TemplateManager extends PKPTemplateManager {
 			$params['context'] = $context;
 		}
 		
-		return parent::smartyUrl($params, &$smarty);
+		return parent::smartyUrl($params, $smarty);
 	}
 
 	/**
