c28c350bf292802ab4a5669c73b71622ce489c1c
asmecher <asmecher>
2009-01-31 00:20:28 +0000

    #4029# Fixed time zone

--- classes/user/User.inc.php
+++ classes/user/User.inc.php
@@ -29,7 +29,7 @@ class User extends PKPUser {
 	}
 
 	function setTimeZone($timeZone) {
-		return $this->updateSetting('timeZone');
+		return $this->updateSetting('timeZone', $timeZone);
 	}
 
 	function getTimeZone() {
