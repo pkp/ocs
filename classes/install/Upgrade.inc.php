813b4cb2a40a9eb55c84a60331a5ff164f0c34f5
mcrider <mcrider>
2009-03-13 19:01:24 +0000

    #4103# homeHeaderLogoImage is not properly localized on upgrade

--- classes/install/Upgrade.inc.php
+++ classes/install/Upgrade.inc.php
@@ -216,6 +216,7 @@ class Upgrade extends Installer {
 			'presenterInformation' => 'presenterInformation',
 			'announcementsIntroduction' => 'announcementsIntroduction',
 			// Setup page 3
+			'homeHeaderLogoImage' => 'homeHeaderLogoImage',
 			'homeHeaderTitleType' => 'homeHeaderTitleType',
 			'homeHeaderTitle' => 'homeHeaderTitle',
 			'homeHeaderTitleImage' => 'homeHeaderTitleImage',
