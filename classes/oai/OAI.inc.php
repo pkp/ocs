ed5954a78fc79c5fc892a98c881491fb044df691
asmecher <asmecher>
2009-07-15 23:38:56 +0000

    #4525# Fixed escaping problem

--- classes/oai/OAI.inc.php
+++ classes/oai/OAI.inc.php
@@ -694,7 +694,7 @@ class OAI {
 			}
 		}
 
-		echo	">" . $this->config->baseUrl . "</request>\n" .
+		echo	">" . $this->prepOutput($this->config->baseUrl) . "</request>\n" .
 			$response .
 			"</OAI-PMH>\n";
 	}
