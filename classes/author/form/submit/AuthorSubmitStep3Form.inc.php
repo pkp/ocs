354565cf24b1254fb94d572f569ecff487a1c773
asmecher <asmecher>
2009-08-28 22:55:30 +0000

    #4694# Fixed ack to track directors

--- classes/author/form/submit/AuthorSubmitStep3Form.inc.php
+++ classes/author/form/submit/AuthorSubmitStep3Form.inc.php
@@ -209,13 +209,13 @@ class AuthorSubmitStep3Form extends AuthorSubmitForm {
 				$paper->setDateSubmitted(Core::getCurrentDate());
 				$paper->stampStatusModified();
 				$paper->setCurrentStage(REVIEW_STAGE_ABSTRACT);
-				$trackDirectors = $this->assignDirectors($paper);
+				$this->assignDirectors($paper);
 
 				if ($schedConf->getSetting('acceptSupplementaryReviewMaterials')) {
 					$paper->setSubmissionProgress($this->step + 2);
 				} else {
 					$paper->setSubmissionProgress(0);
-					$this->confirmSubmission($paper, $user, $schedConf, $conference, 'SUBMISSION_ACK', $trackDirectors);
+					$this->confirmSubmission($paper, $user, $schedConf, $conference, 'SUBMISSION_ACK');
 				}
 			}
 		}
