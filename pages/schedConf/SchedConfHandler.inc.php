aa12c3ee36a66fdd11e344dd792aec4671f96adb
Alec Smecher <alec@smecher.bc.ca>
2010-03-03 13:54:14 +0100

    *5189* Fixed schedConf/conference mix-up for track directors

--- pages/schedConf/SchedConfHandler.inc.php
+++ pages/schedConf/SchedConfHandler.inc.php
@@ -91,7 +91,7 @@ class SchedConfHandler extends Handler {
 		$templateMgr->assign_by_ref('tracks', $tracks);
 		$trackDirectors = array();
 		foreach ($tracks as $track) {
-			$trackDirectors[$track->getId()] =& $trackDirectorsDao->getDirectorsByTrackId($conference->getId(), $track->getId());
+			$trackDirectors[$track->getId()] =& $trackDirectorsDao->getDirectorsByTrackId($schedConf->getId(), $track->getId());
 		}
 		$templateMgr->assign_by_ref('trackDirectors', $trackDirectors);
 
