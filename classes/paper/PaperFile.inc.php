4d6baccee996cf63afd084b8b187b2521c103500
Alec Smecher <alec@smecher.bc.ca>
2010-05-05 13:23:34 -0700

    *5386* Review refactoring continues

--- classes/paper/PaperFile.inc.php
+++ classes/paper/PaperFile.inc.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * @file PaperFile.inc.php
+ * @file classes/paper/PaperFile.inc.php
  *
  * Copyright (c) 2000-2010 John Willinsky
  * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
@@ -13,15 +13,18 @@
  * @brief Paper file class.
  */
 
-//$Id$
+// $Id$
 
-class PaperFile extends DataObject {
+
+import('lib.pkp.classes.submission.SubmissionFile');
+
+class PaperFile extends SubmissionFile {
 
 	/**
 	 * Constructor.
 	 */
 	function PaperFile() {
-		parent::DataObject();
+		parent::SubmissionFile();
 	}
 
 	/**
@@ -42,44 +45,13 @@ class PaperFile extends DataObject {
 	//
 	// Get/set methods
 	//
-	/**
-	 * Get ID of file.
-	 * @return int
-	 */
-	function getFileId() {
-		return $this->getData('fileId');
-	}
-
-	/**
-	 * Set ID of file.
-	 * @param $fileId int
-	 */
-	function setFileId($fileId) {
-		return $this->setData('fileId', $fileId);
-	}
-
-	/**
-	 * Get revision number.
-	 * @return int
-	 */
-	function getRevision() {
-		return $this->getData('revision');
-	}
-
-	/**
-	 * Set revision number.
-	 * @param $revision int
-	 */
-	function setRevision($revision) {
-		return $this->setData('revision', $revision);
-	}
 
 	/**
 	 * Get ID of paper.
 	 * @return int
 	 */
 	function getPaperId() {
-		return $this->getData('paperId');
+		return $this->getSubmissionId();
 	}
 
 	/**
@@ -87,87 +59,7 @@ class PaperFile extends DataObject {
 	 * @param $paperId int
 	 */
 	function setPaperId($paperId) {
-		return $this->setData('paperId', $paperId);
-	}
-
-	/**
-	 * Get file name of the file.
-	 * @param return string
-	 */
-	function getFileName() {
-		return $this->getData('fileName');
-	}
-
-	/**
-	 * Set file name of the file.
-	 * @param $fileName string
-	 */
-	function setFileName($fileName) {
-		return $this->setData('fileName', $fileName);
-	}
-
-	/**
-	 * Get file type of the file.
-	 * @ return string
-	 */
-	function getFileType() {
-		return $this->getData('fileType');
-	}
-
-	/**
-	 * Set file type of the file.
-	 * @param $fileType string
-	 */
-	function setFileType($fileType) {
-		return $this->setData('fileType', $fileType);
-	}
-
-	/**
-	 * Get original uploaded file name of the file.
-	 * @param return string
-	 */
-	function getOriginalFileName() {
-		return $this->getData('originalFileName');
-	}
-
-	/**
-	 * Set original uploaded file name of the file.
-	 * @param $originalFileName string
-	 */
-	function setOriginalFileName($originalFileName) {
-		return $this->setData('originalFileName', $originalFileName);
-	}
-
-	/**
-	 * Get type of the file.
-	 * @ return string
-	 */
-	function getType() {
-		return $this->getData('type');
-	}
-
-	/**
-	 * Set type of the file.
-	 * @param $type string
-	 */
-	function setType($type) {
-		return $this->setData('type', $type);
-	}
-
-	/**
-	 * Get review round of the file.
-	 * @ return int
-	 */
-	function getRound() {
-		return $this->getData('round');
-	}
-
-	/**
-	 * Set round of the file.
-	 * @param $round string
-	 */
-	function setRound($round) {
-		return $this->setData('round', $round);
+		return $this->setSubmissionId($paperId);
 	}
 
 	/**
@@ -186,91 +78,6 @@ class PaperFile extends DataObject {
 	function setStage($stage) {
 		if (Config::getVar('debug', 'deprecation_warnings')) trigger_error('Deprecated function.');
 		return $this->setRound($stage);
-	}
-
-	/**
-	 * Get uploaded date of file.
-	 * @return date
-	 */
-
-	function getDateUploaded() {
-		return $this->getData('dateUploaded');
-	}
-
-
-	/**
-	 * Set uploaded date of file.
-	 * @param $dateUploaded date
-	 */
-
-	function setDateUploaded($dateUploaded) {
-		return $this->SetData('dateUploaded', $dateUploaded);
-	}
-
-	/**
-	 * Get modified date of file.
-	 * @return date
-	 */
-
-	function getDateModified() {
-		return $this->getData('dateModified');
-	}
-
-
-	/**
-	 * Set modified date of file.
-	 * @param $dateModified date
-	 */
-
-	function setDateModified($dateModified) {
-		return $this->SetData('dateModified', $dateModified);
-	}
-
-	/**
-	 * Get file size of file.
-	 * @return int
-	 */
-
-	function getFileSize() {
-		return $this->getData('fileSize');
-	}
-
-
-	/**
-	 * Set file size of file.
-	 * @param $fileSize int
-	 */
-
-	function setFileSize($fileSize) {
-		return $this->SetData('fileSize', $fileSize);
-	}
-
-	/**
-	 * Get nice file size of file.
-	 * @return string
-	 */
-
-	function getNiceFileSize() {
-		return FileManager::getNiceFileSize($this->getData('fileSize'));
-	}
-
-	/**
-	 * Get viewable.
-	 * @return boolean
-	 */
-
-	function getViewable() {
-		return $this->getData('viewable');
-	}
-
-
-	/**
-	 * Set viewable.
-	 * @param $viewable boolean
-	 */
-
-	function setViewable($viewable) {
-		return $this->SetData('viewable', $viewable);
 	}
 
 	/**
