8fd14b0d40017a282511190b78102e923b11af06
michael-pkp <michael.pkp@gmail.com>
2010-11-19 16:48:15 -0800

    *6080* TinyMCE converts to HTML entities

--- plugins/generic/tinymce/TinyMCEPlugin.inc.php
+++ plugins/generic/tinymce/TinyMCEPlugin.inc.php
@@ -285,6 +285,7 @@ class TinyMCEPlugin extends GenericPlugin {
 			</script>
 			<script language="javascript" type="text/javascript">
 				tinyMCE.init({
+					entity_encoding : "raw",
 					plugins : "paste",
 					mode : "exact",
 					language : "' . String::substr(Locale::getLocale(), 0, 2) . '",
