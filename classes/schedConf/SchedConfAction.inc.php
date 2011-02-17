dcc77bb5109ec5d21e6869e786b5bf6a7b40ca56
Alec Smecher <alec@smecher.bc.ca>
2011-02-17 10:33:25 -0800

    *6419* Fixed sched conf links for disabled conferences

--- classes/schedConf/SchedConfAction.inc.php
+++ classes/schedConf/SchedConfAction.inc.php
@@ -70,7 +70,7 @@ class SchedConfAction {
 	 */
 	function mayViewSchedConf(&$schedConf) {
 		$conference =& $schedConf->getConference();
-		return $conference->getEnabled() || Validation::isSiteAdmin() || Validation::isConferenceManager() || Validation::isDirector() || Validation::isTrackDirector();
+		return $conference->getEnabled() || Validation::isSiteAdmin() || Validation::isConferenceManager($conference->getId()) || Validation::isDirector($conference->getId(), $schedConf->getId()) || Validation::isTrackDirector($conference->getId(), $schedConf->getId());
 	}
 
 	/**
