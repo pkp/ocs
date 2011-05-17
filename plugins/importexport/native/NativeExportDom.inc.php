994c15b81f896a6e07e76967a96b10471add2e16
Alec Smecher <alec@smecher.bc.ca>
2011-05-17 07:55:02 -0700

    *6647* Affiliation node typo

--- plugins/importexport/native/NativeExportDom.inc.php
+++ plugins/importexport/native/NativeExportDom.inc.php
@@ -193,7 +193,7 @@ class NativeExportDom {
 		if (is_array($affiliations)) foreach ($affiliations as $locale => $affiliation) {
 			$affiliationNode =& XMLCustomWriter::createChildWithText($doc, $root, 'affiliation', $affiliation, false);
 			if ($affiliationNode) XMLCustomWriter::setAttribute($affiliationNode, 'locale', $locale);
-			unset($n);
+			unset($affiliationNode);
 		}
 
 		XMLCustomWriter::createChildWithText($doc, $root, 'country', $author->getCountry(), false);
