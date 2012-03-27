317aca9be27b6ddb079205f20d57c43ba84cf2cd
Alex <axfelix@gmail.com>
2012-03-27 10:33:52 -0700

    porting Citation Markup Assistant

---
+++ classes/controllers/grid/filter/FilterGridHandler.inc.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * @file classes/controllers/grid/filter/FilterGridHandler.inc.php
+ *
+ * Copyright (c) 2000-2012 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class FilterGridHandler
+ * @ingroup classes_controllers_grid_filter
+ *
+ * @brief Handle OJS specific parts of filter grid requests.
+ */
+
+import('lib.pkp.classes.controllers.grid.filter.PKPFilterGridHandler');
+
+// import validation classes
+import('classes.handler.validation.HandlerValidatorJournal');
+import('lib.pkp.classes.handler.validation.HandlerValidatorRoles');
+
+class FilterGridHandler extends PKPFilterGridHandler {
+	/**
+	 * Constructor
+	 */
+	function FilterGridHandler() {
+		parent::PKPFilterGridHandler();
+		$this->addRoleAssignment(
+				ROLE_ID_JOURNAL_MANAGER,
+				array('fetchGrid', 'addFilter', 'editFilter', 'updateFilter', 'deleteFilter'));
+	}
+
+
+	//
+	// Implement template methods from PKPHandler
+	//
+	/**
+	 * @see PKPHandler::authorize()
+	 */
+	function authorize(&$request, &$args, $roleAssignments) {
+		// Make sure the user can change the journal setup.
+		import('classes.security.authorization.OjsJournalAccessPolicy');
+		$this->addPolicy(new OjsJournalAccessPolicy($request, $roleAssignments));
+		return parent::authorize($request, $args, $roleAssignments);
+	}
+}
