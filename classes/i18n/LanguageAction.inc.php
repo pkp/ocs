4d2166499191acb31b3e2764ea92ca97ff4386f3
Alec Smecher <alec@smecher.bc.ca>
2012-11-23 14:37:44 -0800

    *8030* Centralize language admin

---
+++ classes/i18n/LanguageAction.inc.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @defgroup admin
+ */
+
+/**
+ * @file classes/i18n/LanguageAction.inc.php
+ * @defgroup admin
+ *
+ * Copyright (c) 2000-2012 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class LanguageAction
+ * @ingroup admin
+ *
+ * @brief LanguageAction class.
+ */
+
+define('LANGUAGE_PACK_DESCRIPTOR_URL', 'http://pkp.sfu.ca/ocs/xml/%s/locales.xml');
+define('LANGUAGE_PACK_TAR_URL', 'http://pkp.sfu.ca/ocs/xml/%s/%s.tar.gz');
+
+import('lib.pkp.classes.i18n.PKPLanguageAction');
+
+class LanguageAction extends PKPLanguageAction {
+	/**
+	 * Constructor.
+	 */
+	function LanguageAction() {
+		parent::PKPLanguageAction();
+	}
+}
+
+?>
