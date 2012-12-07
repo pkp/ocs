ccbb5a06d5c1439fd90540c8e6ec334944bb0f4f
Alec Smecher <alec@smecher.bc.ca>
2012-12-07 09:50:28 -0800

    *8058* Centralize profile form

--- classes/user/form/ProfileForm.inc.php
+++ classes/user/form/ProfileForm.inc.php
@@ -12,25 +12,15 @@
  * @brief Form to edit user profile.
  */
 
+import('lib.pkp.classes.user.form.PKPProfileForm');
 
-
-import('lib.pkp.classes.form.Form');
-
-class ProfileForm extends Form {
-
-	/** @var $user object */
-	var $user;
-
+class ProfileForm extends PKPProfileForm {
 	/**
 	 * Constructor.
+	 * @param $user PKPUser
 	 */
-	function ProfileForm() {
-		parent::Form('user/profile.tpl');
-
-		$user =& Request::getUser();
-		$this->user =& $user;
-
-		$site =& Request::getSite();
+	function ProfileForm($user) {
+		parent::PKPProfileForm('user/profile.tpl', $user);
 
 		// Validation checks for this form
 		$this->addCheck(new FormValidator($this, 'firstName', 'required', 'user.profile.form.firstNameRequired'));
@@ -43,77 +33,18 @@ class ProfileForm extends Form {
 	}
 
 	/**
-	 * Deletes a profile image.
-	 */
-	function deleteProfileImage() {
-		$user =& Request::getUser();
-		$profileImage = $user->getSetting('profileImage');
-		if (!$profileImage) return false;
-
-		import('classes.file.PublicFileManager');
-		$fileManager = new PublicFileManager();
-		if ($fileManager->removeSiteFile($profileImage['uploadName'])) {
-			return $user->updateSetting('profileImage', null);
-		} else {
-			return false;
-		}
-	}
-
-	function uploadProfileImage() {
-		import('classes.file.PublicFileManager');
-		$fileManager = new PublicFileManager();
-
-		$user =& $this->user;
-
-		$type = $fileManager->getUploadedFileType('profileImage');
-		$extension = $fileManager->getImageExtension($type);
-		if (!$extension) return false;
-
-		$uploadName = 'profileImage-' . (int) $user->getId() . $extension;
-		if ($fileManager->uploadError('profileImage')) return false;
-		if (!$fileManager->uploadSiteFile('profileImage', $uploadName)) return false;
-
-		$filePath = $fileManager->getSiteFilesPath();
-		list($width, $height) = getimagesize($filePath . '/' . $uploadName);
-
-		if ($width > 150 || $height > 150 || $width <= 0 || $height <= 0) {
-			$userSetting = null;
-			$user->updateSetting('profileImage', $userSetting);
-			$fileManager->removeSiteFile($filePath);
-			return false;
-		}
-
-		$userSetting = array(
-			'name' => $fileManager->getUploadedFileName('profileImage'),
-			'uploadName' => $uploadName,
-			'width' => $width,
-			'height' => $height,
-			'dateUploaded' => Core::getCurrentDate()
-		);
-
-		$user->updateSetting('profileImage', $userSetting);
-		return true;
-	}
-
-	/**
 	 * Display the form.
 	 */
-	function display() {
-		$user =& Request::getUser();
+	function display($request) {
+		$templateMgr = TemplateManager::getManager($request);
 
-		$templateMgr =& TemplateManager::getManager();
-		$templateMgr->assign('username', $user->getUsername());
-
-		$site =& Request::getSite();
-		$templateMgr->assign('availableLocales', $site->getSupportedLocaleNames());
-
-		$roleDao =& DAORegistry::getDAO('RoleDAO');
-		$schedConfDao =& DAORegistry::getDAO('SchedConfDAO');
-		$userSettingsDao =& DAORegistry::getDAO('UserSettingsDAO');
-		$userDao =& DAORegistry::getDAO('UserDAO');
+		$roleDao = DAORegistry::getDAO('RoleDAO');
+		$schedConfDao = DAORegistry::getDAO('SchedConfDAO');
+		$userSettingsDao = DAORegistry::getDAO('UserSettingsDAO');
+		$userDao = DAORegistry::getDAO('UserDAO');
 
 		$schedConfs = $schedConfDao->getAll();
-		$schedConfs =& $schedConfs->toArray();
+		$schedConfs = $schedConfs->toArray();
 
 		foreach ($schedConfs as $thisSchedConf) {
 			if ($thisSchedConf->getSetting('enableOpenAccessNotification') == true) {
@@ -123,19 +54,13 @@ class ProfileForm extends Form {
 			}
 		}
 
-		$countryDao =& DAORegistry::getDAO('CountryDAO');
-		$countries =& $countryDao->getCountries();
-
-		$templateMgr->assign('genderOptions', $userDao->getGenderOptions());
-
 		$templateMgr->assign_by_ref('schedConfs', $schedConfs);
-		$templateMgr->assign_by_ref('countries', $countries);
 		$templateMgr->assign('helpTopicId', 'conference.users.index');
 
-		$schedConf =& Request::getSchedConf();
+		$schedConf = $request->getSchedConf();
 		if ($schedConf) {
-			$roleDao =& DAORegistry::getDAO('RoleDAO');
-			$roles =& $roleDao->getRolesByUserId($user->getId(), $schedConf->getId());
+			$roleDao = DAORegistry::getDAO('RoleDAO');
+			$roles = $roleDao->getRolesByUserId($user->getId(), $schedConf->getId());
 			$roleNames = array();
 			foreach ($roles as $role) $roleNames[$role->getRolePath()] = $role->getRoleName();
 			import('classes.schedConf.SchedConfAction');
@@ -145,148 +70,58 @@ class ProfileForm extends Form {
 			$templateMgr->assign('roles', $roleNames);
 		}
 
-		$timeZoneDao =& DAORegistry::getDAO('TimeZoneDAO');
-		$timeZones =& $timeZoneDao->getTimeZones();
+		$timeZoneDao = DAORegistry::getDAO('TimeZoneDAO');
+		$timeZones = $timeZoneDao->getTimeZones();
 		$templateMgr->assign_by_ref('timeZones', $timeZones);
 
-		$templateMgr->assign('profileImage', $user->getSetting('profileImage'));
-
-		parent::display();
-	}
-
-	function getLocaleFieldNames() {
-		$userDao =& DAORegistry::getDAO('UserDAO');
-		return $userDao->getLocaleFieldNames();
+		parent::display($request);
 	}
 
 	/**
 	 * Initialize form data from current settings.
 	 */
-	function initData(&$args, &$request) {
-		$user =& $request->getUser();
+	function initData() {
+		$user = $this->getUser();
 
 		import('lib.pkp.classes.user.InterestManager');
 		$interestManager = new InterestManager();
 
-		$this->_data = array(
-			'salutation' => $user->getSalutation(),
-			'firstName' => $user->getFirstName(),
-			'middleName' => $user->getMiddleName(),
-			'initials' => $user->getInitials(),
-			'lastName' => $user->getLastName(),
-			'gender' => $user->getGender(),
-			'affiliation' => $user->getAffiliation(null), // Localized
-			'signature' => $user->getSignature(null), // Localized
-			'email' => $user->getEmail(),
-			'userUrl' => $user->getUrl(),
-			'phone' => $user->getPhone(),
-			'fax' => $user->getFax(),
-			'mailingAddress' => $user->getMailingAddress(),
-			'billingAddress' => $user->getBillingAddress(),
-			'country' => $user->getCountry(),
+		parent::initData();
+
+		$this->_data += array(
 			'timeZone' => $user->getTimeZone(),
-			'biography' => $user->getBiography(null), // Localized
-			'userLocales' => $user->getLocales(),
 			'isAuthor' => Validation::isAuthor(),
 			'isReader' => Validation::isReader(),
 			'isReviewer' => Validation::isReviewer(),
-			'interestsKeywords' => $interestManager->getInterestsForUser($user),
-			'interestsTextOnly' => $interestManager->getInterestsString($user)
 		);
-
-
 	}
 
 	/**
 	 * Assign form data to user-submitted data.
 	 */
 	function readInputData() {
+		parent::readInputData();
 		$this->readUserVars(array(
-			'salutation',
-			'firstName',
-			'middleName',
-			'lastName',
-			'gender',
-			'initials',
-			'affiliation',
-			'signature',
-			'email',
-			'userUrl',
-			'phone',
-			'fax',
-			'mailingAddress',
-			'billingAddress',
-			'country',
 			'timeZone',
-			'biography',
-			'keywords',
-			'interestsTextOnly',
-			'userLocales',
 			'readerRole',
 			'authorRole',
 			'reviewerRole'
 		));
-
-		if ($this->getData('userLocales') == null || !is_array($this->getData('userLocales'))) {
-			$this->setData('userLocales', array());
-		}
-
-		$keywords = $this->getData('keywords');
-		if ($keywords != null && is_array($keywords['interests'])) {
-			// The interests are coming in encoded -- Decode them for DB storage
-			$this->setData('interestsKeywords', array_map('urldecode', $keywords['interests']));
-		}
 	}
 
 	/**
 	 * Save profile settings.
 	 */
-	function execute() {
-		$user =& Request::getUser();
+	function execute($request) {
+		$user = $this->getUser();
 
-		$user->setSalutation($this->getData('salutation'));
-		$user->setFirstName($this->getData('firstName'));
-		$user->setMiddleName($this->getData('middleName'));
-		$user->setLastName($this->getData('lastName'));
-		$user->setGender($this->getData('gender'));
-		$user->setInitials($this->getData('initials'));
-		$user->setAffiliation($this->getData('affiliation'), null); // Localized
-		$user->setSignature($this->getData('signature'), null); // Localized
-		$user->setEmail($this->getData('email'));
-		$user->setUrl($this->getData('userUrl'));
-		$user->setPhone($this->getData('phone'));
-		$user->setFax($this->getData('fax'));
-		$user->setMailingAddress($this->getData('mailingAddress'));
-		$user->setBillingAddress($this->getData('billingAddress'));
-		$user->setCountry($this->getData('country'));
 		$user->setTimeZone($this->getData('timeZone'));
-		$user->setBiography($this->getData('biography'), null); // Localized
 
-		// Insert the user interests
-		$interests = $this->getData('interestsKeywords') ? $this->getData('interestsKeywords') : $this->getData('interestsTextOnly');
-		import('lib.pkp.classes.user.InterestManager');
-		$interestManager = new InterestManager();
-		$interestManager->setInterestsForUser($user, $interests);
-
-		$site =& Request::getSite();
-		$availableLocales = $site->getSupportedLocales();
-
-		$locales = array();
-		foreach ($this->getData('userLocales') as $locale) {
-			if (AppLocale::isLocaleValid($locale) && in_array($locale, $availableLocales)) {
-				array_push($locales, $locale);
-			}
-		}
-		$user->setLocales($locales);
-
-		$userDao =& DAORegistry::getDAO('UserDAO');
-		$userDao->updateObject($user);
-
-		$roleDao =& DAORegistry::getDAO('RoleDAO');
-		$schedConfDao =& DAORegistry::getDAO('SchedConfDAO');
+		$roleDao = DAORegistry::getDAO('RoleDAO');
+		$schedConfDao = DAORegistry::getDAO('SchedConfDAO');
 
 		// Roles
-		$schedConf =& Request::getSchedConf();
+		$schedConf = Request::getSchedConf();
 		if ($schedConf) {
 			import('classes.schedConf.SchedConfAction');
 			$role = new Role();
@@ -318,9 +153,9 @@ class ProfileForm extends Form {
 
 		$openAccessNotify = Request::getUserVar('openAccessNotify');
 
-		$userSettingsDao =& DAORegistry::getDAO('UserSettingsDAO');
+		$userSettingsDao = DAORegistry::getDAO('UserSettingsDAO');
 		$schedConfs = $schedConfDao->getAll();
-		$schedConfs =& $schedConfs->toArray();
+		$schedConfs = $schedConfs->toArray();
 
 		foreach ($schedConfs as $thisSchedConf) {
 			if ($thisSchedConf->getSetting('enableOpenAccessNotification') == true) {
@@ -332,14 +167,7 @@ class ProfileForm extends Form {
 			}
 		}
 
-		if ($user->getAuthId()) {
-			$authDao =& DAORegistry::getDAO('AuthSourceDAO');
-			$auth =& $authDao->getPlugin($user->getAuthId());
-		}
-
-		if (isset($auth)) {
-			$auth->doSetUserInfo($user);
-		}
+		parent::execute($request);
 	}
 }
 
